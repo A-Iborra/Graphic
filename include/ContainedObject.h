8cb6208bf90976aa416350ee21a5c7bc558adec9
Nate <nate@useclark.com>
2017-12-05 22:42:18 -0500

    Initial checkin

--- include/ContainedObject.h
+++ include/ContainedObject.h
@@ -3,45 +3,117 @@
 
 #include "ocidl.h"
 
-  template<class Parent> class ContainedObject : public IOleInPlaceSite, public IOleClientSite {
+  class G;
+
+  class ContainedObject : public IUnknown {
 
   public:
 
-     ContainedObject(Parent* pParent,HWND hwndTab = NULL,IUnknown* pIUnknownObject = NULL);
+     ContainedObject(G* pParent,HWND hwndTab,IUnknown* pIUnknownObject,REFIID riidEventsInterface);
      ~ContainedObject();
+
+     int connectEvents();
+     int unConnectEvents();
 
      void ReSize();
 
      HWND HWNDSite() { return hwndSite; };
 
-     virtual int connectEvents() = 0;
-     virtual int unConnectEvents() = 0;
+   // IUnknown
+
+     STDMETHOD(QueryInterface)(REFIID riid,void **ppv);
+
+     STDMETHOD_ (ULONG, AddRef)();
+     STDMETHOD_ (ULONG, Release)();
 
   private:
 
    // IOleInPlaceSite
 
-     STDMETHOD(GetWindow)(HWND*);
-     STDMETHOD(ContextSensitiveHelp)(BOOL);
-     STDMETHOD(CanInPlaceActivate)();
-     STDMETHOD(OnInPlaceActivate)();
-     STDMETHOD(OnUIActivate)();
-     STDMETHOD(GetWindowContext)(IOleInPlaceFrame**,IOleInPlaceUIWindow**,RECT* position,RECT* clip,OLEINPLACEFRAMEINFO*);
-     STDMETHOD(Scroll)(SIZE);
-     STDMETHOD(OnUIDeactivate)(BOOL);
-     STDMETHOD(OnInPlaceDeactivate)();
-     STDMETHOD(DiscardUndoState)();
-     STDMETHOD(DeactivateAndUndo)();
-     STDMETHOD(OnPosRectChange)(const RECT*);
+     class _IOleInPlaceSite : public IOleInPlaceSite {
+
+     public:
+
+        _IOleInPlaceSite(ContainedObject* pp) : pParent(pp) {};
+
+        STDMETHOD(QueryInterface)(REFIID riid,void **ppv);
+
+     private:
+
+        STDMETHOD_ (ULONG, AddRef)();
+        STDMETHOD_ (ULONG, Release)();
+   
+        STDMETHOD(GetWindow)(HWND*);
+        STDMETHOD(ContextSensitiveHelp)(BOOL);
+        STDMETHOD(CanInPlaceActivate)();
+        STDMETHOD(OnInPlaceActivate)();
+        STDMETHOD(OnUIActivate)();
+        STDMETHOD(GetWindowContext)(IOleInPlaceFrame**,IOleInPlaceUIWindow**,RECT* position,RECT* clip,OLEINPLACEFRAMEINFO*);
+        STDMETHOD(Scroll)(SIZE);
+        STDMETHOD(OnUIDeactivate)(BOOL);
+        STDMETHOD(OnInPlaceDeactivate)();
+        STDMETHOD(DiscardUndoState)();
+        STDMETHOD(DeactivateAndUndo)();
+        STDMETHOD(OnPosRectChange)(const RECT*);
+
+      protected:
+
+        ContainedObject* pParent;
+
+        friend class ContainedObject;
+
+     } * pIOleInPlaceSite;
 
 //     IOleClientSite
 
-     STDMETHOD(SaveObject)();
-     STDMETHOD(GetMoniker)(DWORD,DWORD,IMoniker**);
-     STDMETHOD(GetContainer)(IOleContainer**);
-     STDMETHOD(ShowObject)();
-     STDMETHOD(OnShowWindow)(BOOL);
-     STDMETHOD(RequestNewObjectLayout)();
+     class _IOleClientSite : public IOleClientSite {
+
+     public:
+
+        _IOleClientSite(ContainedObject* pp);
+
+        STDMETHOD(QueryInterface)(REFIID riid,void **ppv);
+
+        STDMETHOD_ (ULONG, AddRef)();
+        STDMETHOD_ (ULONG, Release)();
+
+     private:
+
+        STDMETHOD(SaveObject)();
+        STDMETHOD(GetMoniker)(DWORD,DWORD,IMoniker**);
+        STDMETHOD(GetContainer)(IOleContainer**);
+        STDMETHOD(ShowObject)();
+        STDMETHOD(OnShowWindow)(BOOL);
+        STDMETHOD(RequestNewObjectLayout)();
+
+        class _IDispatch : public IDispatch {
+
+        public:
+
+           _IDispatch(_IOleClientSite *pp) : pParent(pp) {};
+
+           STDMETHOD(QueryInterface)(REFIID riid,void **ppv);
+           STDMETHOD_ (ULONG, AddRef)();
+           STDMETHOD_ (ULONG, Release)();
+
+        private:
+
+           STDMETHOD(GetTypeInfoCount)(UINT *pctinfo);
+           STDMETHOD(GetTypeInfo)(UINT itinfo, LCID lcid, ITypeInfo **pptinfo);
+           STDMETHOD(GetIDsOfNames)(REFIID riid, LPOLESTR *rgszNames, UINT cNames, LCID lcid, DISPID *rgdispid);
+           STDMETHOD(Invoke)(DISPID dispidMember, REFIID riid, LCID lcid, WORD wFlags, DISPPARAMS *pdispparams, VARIANT *pvarResult, EXCEPINFO *pexcepinfo, UINT *puArgErr);
+
+           _IOleClientSite *pParent;
+
+         } iDispatch;
+
+      protected:
+
+        ContainedObject* pParent;
+
+        friend class ContainedObject;
+
+     } * pIOleClientSite;
 
    protected:
 
@@ -49,10 +121,14 @@
 
      unsigned int refCount;
      HWND hwndTab,hwndSite;
-     Parent* pParent;
+     G* pParent;
 
+     REFIID riidEventsInterface;
      IConnectionPoint* pIConnectionPoint;
      DWORD dwConnectionCookie;
+
+     friend class _IOleInPlaceSite;
+     friend class _IOleClientSite;
 
   };
 
